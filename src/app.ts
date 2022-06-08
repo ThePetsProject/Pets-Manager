c5b80423f709dcc553d79ada5a8242885c3314f2
janovoas <javiernovoa.s@gmail.com>
2022-06-08 12:47:22 -0400

    Added v1 to api route and removed slash from route

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,6 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use('/api', routes)
+app.use('/api/v1/', routes)
 
 export default app
--- src/routes/example/index.ts
+++ src/routes/example/index.ts
@@ -2,7 +2,7 @@ import express, { Router } from 'express'
 const router = express.Router()
 
 export const exampleRoute = (): Router => {
-  return router.get('/example', (req, res) => {
+  return router.get('example', (req, res) => {
     return res.status(200).send('This is the example route response')
   })
 }
