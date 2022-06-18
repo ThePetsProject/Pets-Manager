89d07939a3a4f0bf099c8c10f5af1f6f466bb6f7
janovoas <javiernovoa.s@gmail.com>
2022-06-18 12:07:42 -0400

    Jest refactor

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,42 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  setupFiles: ['dotenv/config'],
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['build/'],
-  coveragePathIgnorePatterns: [
-    'node_modules',
-    './src/infrastructure/database/models/*',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/$1',
+    '^@utils/(.*)$': '<rootDir>/utils/$1',
+    '^@database/(.*)$': '<rootDir>/infrastructure/database/$1',
+  },
+  rootDir: 'src',
+  bail: 1,
+  verbose: true,
+  globals: {
+    'ts-jest': {
+      astTransformers: {
+        before: ['ts-nameof'],
+      },
+    },
+  },
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '**/infrastructure/**',
+    '**/infrastructure/database/**',
+    '!./src/routes/health/**',
+    '!**/node_modules/**',
+    '!./index.ts',
+    '!./app.ts',
   ],
-  // testMatch: ['!./src/app.ts'],
+  coveragePathIgnorePatterns: ['.*__snapshots__/.*'],
+  coverageDirectory: '../coverage',
   coverageThreshold: {
     global: {
-      branches: 20,
-      functions: 30,
-      lines: 50,
-      statements: 50,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 }
