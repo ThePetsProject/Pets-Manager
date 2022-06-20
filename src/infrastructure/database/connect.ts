3b7d57562e8b4b23823022fd62de72aa41954d7e
janovoas <javiernovoa.s@gmail.com>
2022-06-20 11:27:38 -0400

    Fixed

--- src/infrastructure/database/connect.ts
+++ src/infrastructure/database/connect.ts
@@ -4,9 +4,9 @@ export const mongoConnect = async (): Promise<string> => {
   const mongooseOptions: mongoose.ConnectOptions = {}
   const ddbb = 'tpp'
   const authSource = 'admin'
-  const mongo = `mongodb://admin:adm@localhost:50112/${ddbb}?authSource=${authSource}`
-  // const mongo =
-  //   'mongodb+srv://tpp-login-manager:K4YF2qx3b5O07B16@private-thepetsproject-mongodb-e6963660.mongo.ondigitalocean.com/tpp?tls=true&authSource=admin&replicaSet=thepetsproject-mongodb&tlsCAFile=/etc/certs/mongo-ca-certificate.cer'
+  // const mongo = `mongodb://admin:adm@localhost:50112/${ddbb}?authSource=${authSource}`
+  const mongo =
+    'mongodb+srv://tpp-login-manager:K4YF2qx3b5O07B16@private-thepetsproject-mongodb-e6963660.mongo.ondigitalocean.com/tpp?tls=true&authSource=admin&replicaSet=thepetsproject-mongodb&tlsCAFile=/etc/certs/mongo-ca-certificate.cer'
   return new Promise((resolve, reject) => {
     mongoose.connect(mongo, mongooseOptions).then(
       () => {
