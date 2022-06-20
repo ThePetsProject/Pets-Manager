0d378d3e82797832ca647a30fdf7b18d11c31485
janovoas <javiernovoa.s@gmail.com>
2022-06-19 22:06:30 -0400

    Commented mongo connect for testing - Rollback

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { mongoConnect } from './infrastructure/database/connect'
 
 const port = process.env.PORT || 3100
 
-// mongoConnect().then(() => {
-// })
-app.listen(port, async () => {
-  console.log(`server is listening on ${port}`)
+mongoConnect().then(() => {
+  app.listen(port, async () => {
+    console.log(`server is listening on ${port}`)
+  })
 })
