c4e9944f1532095755ac959ade1acb69e3e55411
janovoas <javiernovoa.s@gmail.com>
2022-06-20 11:40:27 -0400

    mongostring now retrieved from vault generated file

--- src/infrastructure/database/connect.ts
+++ src/infrastructure/database/connect.ts
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose'
+import fs from 'fs'
 
 export const mongoConnect = async (): Promise<string> => {
   const mongooseOptions: mongoose.ConnectOptions = {}
-  const ddbb = 'tpp'
-  const authSource = 'admin'
-  // const mongo = `mongodb://admin:adm@localhost:50112/${ddbb}?authSource=${authSource}`
-  const mongo =
-    'mongodb+srv://tpp-login-manager:K4YF2qx3b5O07B16@private-thepetsproject-mongodb-e6963660.mongo.ondigitalocean.com/tpp?tls=true&authSource=admin&replicaSet=thepetsproject-mongodb&tlsCAFile=/etc/certs/mongo-ca-certificate.cer'
+  const mongoStrinfFilePath = process.env.MONGO_STRING_FILE_PATH
+  const mongoString = fs
+    .readFileSync(`${mongoStrinfFilePath}mongostring.txt`)
+    .toString()
   return new Promise((resolve, reject) => {
-    mongoose.connect(mongo, mongooseOptions).then(
+    mongoose.connect(mongoString, mongooseOptions).then(
       () => {
         console.log('Connected to Mongo')
         return resolve('')
