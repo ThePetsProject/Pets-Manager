3f536e484867797d044e21632b54b40d7af78fbb
janovoas <javiernovoa.s@gmail.com>
2022-07-02 22:18:54 -0400

    First

--- src/infrastructure/routes/login/index.ts
+++ src/infrastructure/routes/login/index.ts
@@ -22,7 +22,7 @@ export const loginHandler = async (
 ): Promise<Response> => {
   const { email, password } = req.body
 
-  console.info(`[ACC-LOGIN-MANAGER] Trying login for ${email}`)
+  console.info(`[ACC-MANAGER] Trying login for ${email}`)
 
   const schema = Joi.object({
     email: Joi.string().email().required(),
@@ -32,9 +32,7 @@ export const loginHandler = async (
   const { error, value } = schema.validate({ email, password })
 
   if (error) {
-    console.error(
-      `[ACC-LOGIN-MANAGER][SCHEMA_ERROR][ERROR_MESSAGE] ${error.message}`
-    )
+    console.error(`[ACC-MANAGER][SCHEMA_ERROR][ERROR_MESSAGE] ${error.message}`)
     return res.status(400).send()
   }
 
@@ -44,7 +42,7 @@ export const loginHandler = async (
 
   if (!userExists) {
     console.error(
-      `[ACC-LOGIN-MANAGER][USER_ERROR][USER_NOT_FOUND] User ${email} not found`
+      `[ACC-MANAGER][USER_ERROR][USER_NOT_FOUND] User ${email} not found`
     )
     return res.status(404).send()
   }
@@ -53,7 +51,7 @@ export const loginHandler = async (
 
   if (!pwdCheck) {
     console.error(
-      `[ACC-LOGIN-MANAGER][USER_ERROR][WRONG_PASSWORD] Wrong password for ${email}`
+      `[ACC-MANAGER][USER_ERROR][WRONG_PASSWORD] Wrong password for ${email}`
     )
     return res.status(401).send()
   }
@@ -78,11 +76,9 @@ export const loginHandler = async (
     })
     .catch((error: AxiosError) => {
       console.error(
-        `[ACC-LOGIN-MANAGER][AXIOS_ERROR][ERROR_MESSAGE] ${error.message}`
+        `[ACC-MANAGER][AXIOS_ERROR][ERROR_MESSAGE] ${error.message}`
       )
-      console.error(
-        `[ACC-LOGIN-MANAGER][AXIOS_ERROR][ERROR_CODE] ${error.code}`
-      )
+      console.error(`[ACC-MANAGER][AXIOS_ERROR][ERROR_CODE] ${error.code}`)
       return res.status(500).send({
         message: error.message,
       })
